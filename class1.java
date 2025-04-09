39c33adac29096da695257d6d47a78a49c0d7289
Anand7879 <Anand7879@users.noreply.github.com>
2025-04-09 10:48:46 +0530

    Day1

---
+++ class1.java
@@ -0,0 +1,36 @@
+public class class1 {
+    public static void main(String[] args) {
+        System.out.println("hello");
+        // byte range -128 to 127
+
+        // typeConversion
+//        int b = 5;
+//        byte a = (byte)b;
+//        System.out.println(a);
+
+//        byte a = 10;
+//        byte b = 20;
+//        boolean c = true;
+//        boolean d = true;
+//        // byte c = a+b; // by default + operator always give the integer value
+//       byte  c = (byte) (a+b);
+//        System.out.println(c);
+//        System.out.println(10+10+"hello");
+//        System.out.println(5<6);
+//        System.out.println(a & b);
+//
+         // If else
+
+//        boolean a = true;
+//        if(a == true)
+//        {
+//            System.out.println("hello");
+//        }else{
+//            System.out.println("Hii");
+//        }
+
+        System.out.println(5<6?"hello":"Hii");
+
+
+    }
+}
